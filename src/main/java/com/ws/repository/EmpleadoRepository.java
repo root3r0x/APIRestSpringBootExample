4559af0542fbe64f207d33c96a5dbaf62326d71f
Migue LD <ledesma.duarte@duck.com>
2022-12-17 00:49:11 -0600

    Repo Empleado creado[ok]

--- src/main/java/com/ws/repository/EmpleadoRepository.java
+++ src/main/java/com/ws/repository/EmpleadoRepository.java
@@ -1,5 +1,11 @@
 package com.ws.repository;
 
-public class EmpleadoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ws.entity.Empleado;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer>{
 
 }
