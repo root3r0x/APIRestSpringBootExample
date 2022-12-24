370c93a9554be81d32e57cefe1e36595f68b9760
Migue LD <ledesmaduarte@duck.com>
2022-12-24 02:35:34 -0600

    Se agrega configuración global de CORS

--- src/main/java/com/ws/WsTutorialApplication.java
+++ src/main/java/com/ws/WsTutorialApplication.java
@@ -3,11 +3,26 @@ package com.ws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class WsTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsTutorialApplication.class, args);
+	}
+	
+	//Configuracion de CORS global /url en este caso /** para raiz del proyecto
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+			}
+		};
 	}
 
 }
